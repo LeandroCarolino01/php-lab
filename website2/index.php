07b6109a32650dc826b74836d30972c3702dad76
Leandro.Carolino <carolino.leandro@gmail.com>
2018-12-25 19:15:46 +0100

    server and client listing

--- website2/index.php
+++ website2/index.php
@@ -1,10 +1,40 @@
-<?php
+<?php include 'server-info.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>System Info</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Server & File Info</h1>
+        <?php if($server): ?>
+            <ul class="list-group">
+                <?php foreach($server as $key => $value): ?>
+                    <li class="list-group-item">
+                        <strong><?php echo $key; ?></strong>
+                        <?php echo $value; ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
 
-    $server = [
-        'host server name' => $_SERVER['SERVER_NAME'],
-        'Host Header' => $_SERVER['HTTP_HOST'],
-        'Server Software' => $_SERVER['SERVER_SOFTWARE']
-    
-    ];
+        <h1>CLient Info</h1>
+        <?php if($client): ?>
+            <ul class="list-group">
+                <?php foreach($client as $key => $value): ?>
+                    <li class="list-group-item">
+                        <strong><?php echo $key; ?></strong>
+                        <?php echo $value; ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
 
-    echo $server['Server Software'];
+        
+    </div>
+</body>
+</html>
